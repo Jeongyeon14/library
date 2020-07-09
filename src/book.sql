c415b98a2baa17e41d9b6369f200f9687092dc0d
Jeongyeon14 <jeongyeon9314@naver.com>
2020-07-09 15:54:42 +0900

    sql 수정

--- src/book.sql
+++ src/book.sql
@@ -5,9 +5,10 @@ book_writer VARCHAR2(50), --작가명
 book_isbn NUMBER(20,0), --ISBN
 book_publisher VARCHAR2(50), --출판사
 book_number NUMBER(20,0),  --도서번호
-book_date NUMBER(20,0), --발행일
+book_date VARCHAR2(50), --발행일
 PRIMARY KEY(book_name)
 );
+
 
 CREATE TABLE member(
 user_id VARCHAR2(50), 
@@ -22,7 +23,7 @@ PRIMARY KEY(user_id)
 CREATE TABLE rental(
 rental_bookname VARCHAR2(50), --대여 도서명
 rental_userid VARCHAR2(50), --대여 회원
-rental_booknumer NUMBER(20,0), --대여 도서번호
+rental_booknumer VARCHAR2(50), --대여 도서번호
 rental_date NUMBER(20,0), --대여일자
 PRIMARY KEY(rental_bookname)
 );
